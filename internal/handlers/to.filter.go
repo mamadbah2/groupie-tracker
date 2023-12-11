63725ccac79cca2f04c1b761e64328d142a8b40a
mamadbah <bahmamadoubobosewa@gmail.com>
2023-12-11 15:54:26 +0000

    fin definition foncion

---
+++ internal/handlers/to.filter.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "net/http"
+
+func Filter(w http.ResponseWriter, r *http.Request) {
+
+
+}
---
+++ internal/pkg/the.filter.go
@@ -0,0 +1,23 @@
+package pkg
+
+import "bim/internal/models"
+
+func FilterByMember(number int) []models.Artists {
+
+	return []models.Artists{}
+}
+
+func FilterByDateCreation(date int) []models.Artists {
+
+	return []models.Artists{}
+}
+
+func FilterByFirstAlbum(albumName string) []models.Artists {
+
+	return []models.Artists{}
+}
+
+func FilterByLocation(locate string) []models.Artists {
+
+	return []models.Artists{}
+}
